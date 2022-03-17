be176397823a8fd7620284da341b27858356c8fc
fabseman <fabse_utilities@pm.me>
2022-03-17 17:12:23 +0000

    Expanded nopasswd, when executing external script with sudo

--- install_artix.sh
+++ install_artix.sh
@@ -1502,14 +1502,14 @@ EOF
       if [[ "$REPLACE_sudo" == "true" ]]; then
         echo "permit nopass $USERNAME" | tee -a /etc/doas.conf > /dev/null
       else
-        echo "username ALL=(ALL) NOPASSWD: $basename_clean/$POST_install_script_name" | tee -a /etc/sudoers > /dev/null
+        echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" | tee -a /etc/sudoers > /dev/null
       fi
       su -l "$USERNAME" -c "git clone https://$POST_install_script; cd "$basename_clean"; chmod u+x "$POST_install_script_name"; bash "$POST_install_script_name""
       rm -rf /home/$USERNAME/$basename_clean
       if [[ "$REPLACE_sudo" == "true" ]]; then
         sed -i "/permit nopass $USERNAME/d" /etc/doas.conf
       else
-        sed -i "/username ALL=(ALL) NOPASSWD: $basename_clean\/$POST_install_script_name/d" /etc/sudoers
+        sed -i "/$USERNAME ALL=(ALL) NOPASSWD: ALL/d" /etc/sudoers
       fi
     fi
 }
