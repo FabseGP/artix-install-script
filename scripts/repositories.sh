88429b6c3158cdecae80a3e441b3859b5c4f829f
fabseman <fabse_utilities@pm.me>
2022-08-04 11:09:46 +0000

    Fixed installing artix-archlinux-support

--- scripts/repositories.sh
+++ scripts/repositories.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-  pacman -Sy --noconfirm artix-archlinux-support
+  cp configs/pacman2.conf /etc/pacman.conf
+  pacman -Syy --noconfirm artix-archlinux-support
   pacman-key --init
   pacman-key --populate archlinux artix
   if [[ "$(find /install_script/configs -name pacman.conf)" ]]; then
