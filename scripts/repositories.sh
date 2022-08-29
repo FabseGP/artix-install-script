17695ea2533d85cc2b612d5c72ba2efec72846cb
fabseman <4969281-FabseGP02@users.noreply.gitlab.com>
2022-08-29 18:23:14 +0000

    Small fix (again again)

--- scripts/repositories.sh
+++ scripts/repositories.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
   if [[ "$(find /install_script/configs -name pacman.conf)" ]]; then
     cp /install_script/configs/pacman2.conf /etc/pacman.conf
+    pacman -Sy --noconfirm artix-keyring
+    rm -r /etc/pacman.d/gnupg
     pacman-key --init
     pacman-key --populate artix
     pacman-key --refresh-keys
