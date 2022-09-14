1a00e9064d9543613e3fc0790bd5e05bdd2046c8
fabseman <4969281-FabseGP02@users.noreply.gitlab.com>
2022-09-14 13:00:34 +0000

    Replaced swap with zram

--- scripts/btrfs_scrub.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Perform a btrfs scrub on the root filesystem
-  btrfs scrub start -c 2 -n 7 /
--- scripts/ssd_trim.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Perform periodic trim
-  /usr/bin/fstrim -av > /var/log/trim.log || true
