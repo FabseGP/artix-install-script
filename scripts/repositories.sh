bb6d6908673e8983a68b33c667cb392b4c487bd7
fabseman <fabse_utilities@pm.me>
2022-09-20 14:33:19 +0000

    Replaced arch's repo

--- scripts/repositories.sh
+++ scripts/repositories.sh
@@ -6,14 +6,20 @@
     pacman-key --init
     pacman-key --populate artix
     pacman-key --refresh-keys
-  fi
-  pacman -Syy --noconfirm artix-archlinux-support
-  pacman-key --init
-  pacman-key --populate archlinux artix
-  if [[ "$(find /install_script/configs -name pacman.conf)" ]]; then
+    pacman -Sy --noconfirm artix-archlinux-support
+    pacman-key --init
+    pacman-key --populate archlinux artix
+    cd /install_script/packages || exit
+    ALPH_mirrorlist="$(ls -- *alhp-mirrorlist-*)"
+    ALPH_keyring="$(ls -- *alhp-keyring-*)"
+    pacman -U --noconfirm $ALPH_mirrorlist $ALPH_keyring
     cp /install_script/configs/pacman.conf /etc/pacman.conf
     pacman -Syy 
-  else
+  else 
+    pacman -Syy --noconfirm artix-archlinux-support
+    pacman-key --init
+    pacman-key --populate archlinux artix
     cp configs/pacman.conf /etc/pacman.conf
     pacman -Syy 
   fi
+
