2b78eec79e514b197c10e977843973e14e499308
fabseman <4969281-FabseGP02@users.noreply.gitlab.com>
2022-09-20 14:16:12 +0000

    Changed kernel-type

--- install_artix.sh
+++ install_artix.sh
@@ -1324,8 +1324,8 @@ EOM
       pacman-key --lsign-key FBA220DFC880C036
       pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
       cp configs/pacman_chaotic.conf /etc/pacman.conf
-      kernel="linux-tkg-cfs"
-      kernel_headers="linux-tkg-cfs-headers"
+      kernel="linux-tkg-cfs-generic_v3"
+      kernel_headers="linux-tkg-cfs-generic_v3-headers"
     else
       kernel="linux-zen"
       kernel_headers="linux-zen-headers"
