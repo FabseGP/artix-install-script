df5e45fe827ab43ab83a4904b18a8c0d9b00de91
fabseman <fabsegp@fabseman.space>
2022-10-05 13:42:09 +0200

    Better syntax

--- scripts/output.sh
+++ scripts/output.sh
@@ -39,13 +39,8 @@
 # Functions to print text / outputs of commands / lines
 
   PRINT_LINES() {
-  if [[ "$1" == "SPACE" ]]; then
-    echo
-    printf '%0.s-' $(seq 1 "$COLUMNS")
-    echo
-  else
-    printf '%0.s-' $(seq 1 "$COLUMNS")
-  fi
+  if [[ "$1" == "SPACE" ]]; then echo; printf '%0.s-' $(seq 1 "$COLUMNS"); echo;
+  else printf '%0.s-' $(seq 1 "$COLUMNS"); fi
 }
 
   PRINT_MESSAGE() {
@@ -72,29 +67,20 @@
     if ! [[ "${delimiter}" == "" ]] && [[ "$(EMPTY_STRING "${data}")" = "false" ]]; then
       local -r numberOfLines="$(wc -l <<< "${data}")"
       if [[ "${numberOfLines}" -gt "0" ]]; then
-        local table=""
-        local i=1
+        local table="" && local i=1
         for ((i=1; i<="${numberOfLines}"; i++)); do
           local line=""
           line="$(sed "${i}q;d" <<< "${data}")"
           local numberOfColumns="0"
           numberOfColumns="$(awk -F "${delimiter}" '{print NF}' <<< "${line}")"
-          if [[ "${i}" -eq "1" ]]; then
-            table="${table}$(printf '%s#+' "$(REPEAT_STRING '#+' "${numberOfColumns}")")"
-          fi
+          if [[ "${i}" -eq "1" ]]; then table="${table}$(printf '%s#+' "$(REPEAT_STRING '#+' "${numberOfColumns}")")"; fi
           table="${table}\n"
           local j=1
-          for ((j=1; j<="${numberOfColumns}"; j++)); do
-            table="${table}$(printf '#| %s' "$(cut -d "${delimiter}" -f "${j}" <<< "${line}")")"
-          done
+          for ((j=1; j<="${numberOfColumns}"; j++)); do table="${table}$(printf '#| %s' "$(cut -d "${delimiter}" -f "${j}" <<< "${line}")")"; done
           table="${table}#|\n"
-          if [[ "${i}" -eq "1" ]] || [[ "${numberOfLines}" -gt "1" && "${i}" -eq "${numberOfLines}" ]]; then
-            table="${table}$(printf '%s#+' "$(REPEAT_STRING '#+' "${numberOfColumns}")")"
-          fi
+          if [[ "${i}" -eq "1" ]] || [[ "${numberOfLines}" -gt "1" && "${i}" -eq "${numberOfLines}" ]]; then table="${table}$(printf '%s#+' "$(REPEAT_STRING '#+' "${numberOfColumns}")")"; fi
         done
-        if [[ "$(EMPTY_STRING "${table}")" = "false" ]]; then
-          echo -e "${table}" | column -s '#' -t | awk '/^\+/{gsub(" ", "-", $0)}1'
-        fi
+        if [[ "$(EMPTY_STRING "${table}")" = "false" ]]; then echo -e "${table}" | column -s '#' -t | awk '/^\+/{gsub(" ", "-", $0)}1'; fi
       fi
     fi
 }
@@ -107,17 +93,12 @@
   REPEAT_STRING() {
     local -r string="${1}"
     local -r numberToRepeat="${2}"
-    if ! [[ "${string}" == "" ]] && [[ "${numberToRepeat}" =~ ^[1-9][0-9]*$ ]]; then
-      local -r result="$(printf "%${numberToRepeat}s")"
-      echo -e "${result// /${string}}"
-    fi
+    if ! [[ "${string}" == "" ]] && [[ "${numberToRepeat}" =~ ^[1-9][0-9]*$ ]]; then local -r result="$(printf "%${numberToRepeat}s")"; echo -e "${result// /${string}}"; fi
 }
 
   EMPTY_STRING() {
     local -r string="${1}"
-    if [[ "$(TRIM_STRING "${string}")" = "" ]]; then
-      echo 'true' && return 0
-    fi
+    if [[ "$(TRIM_STRING "${string}")" = "" ]]; then echo 'true' && return 0; fi
     echo 'false' && return 1
 }
 
--- scripts/repositories.sh
+++ scripts/repositories.sh
@@ -5,18 +5,10 @@
     cp /install_script/configs/pacman_without_arch.conf /etc/pacman.conf
     pacman -Sy --noconfirm artix-keyring
     rm -r /etc/pacman.d/gnupg
-    pacman-key --init
-    pacman-key --populate artix
-    pacman-key --refresh-keys
-    pacman -Sy --noconfirm artix-archlinux-support
-    pacman-key --init
-    pacman-key --populate archlinux artix
-    cp /install_script/configs/pacman_with_arch.conf /etc/pacman.conf
-    pacman -Sy
+    pacman-key --init && pacman-key --populate artix && pacman-key --refresh-keys
+    pacman -Sy --noconfirm artix-archlinux-support && pacman-key --init && pacman-key --populate archlinux artix
+    cp /install_script/configs/pacman_with_arch.conf /etc/pacman.conf && pacman -Sy
   else 
-    pacman -Syy --noconfirm artix-archlinux-support
-    pacman-key --init
-    pacman-key --populate archlinux artix
-    cp configs/pacman_with_arch.conf /etc/pacman.conf
-    pacman -Sy
+    pacman -Syy --noconfirm artix-archlinux-support && pacman-key --init && pacman-key --populate archlinux artix
+    cp configs/pacman_with_arch.conf /etc/pacman.conf && pacman -Sy
   fi
