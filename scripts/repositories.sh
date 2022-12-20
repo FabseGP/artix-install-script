0385df4cd5a9164420f343d528812e77181b92e5
fabseman <fabsegp@fabseman.space>
2022-12-20 13:18:30 +0100

    Major refactoring

--- scripts/repositories.sh
+++ scripts/repositories.sh
@@ -7,8 +7,17 @@
     rm -r /etc/pacman.d/gnupg
     pacman-key --init && pacman-key --populate artix && pacman-key --refresh-keys
     pacman -Sy --noconfirm artix-archlinux-support && pacman-key --init && pacman-key --populate archlinux artix
+    cd /install_script/packages
+    KEYRING="$(ls -- *alhp-keyring-*)"
+    MIRRORLIST="$(ls -- *alhp-mirrorlist-*)"   
+    pacman -U --noconfirm $KEYRING $MIRRORLIST
     cp /install_script/configs/pacman_with_arch.conf /etc/pacman.conf && pacman -Sy
   else 
     pacman -Syy --noconfirm artix-archlinux-support && pacman-key --init && pacman-key --populate archlinux artix
+    cd packages
+    KEYRING="$(ls -- *alhp-keyring-*)"
+    MIRRORLIST="$(ls -- *alhp-mirrorlist-*)"   
+    pacman -U --noconfirm $KEYRING $MIRRORLIST
+    cd ..
     cp configs/pacman_with_arch.conf /etc/pacman.conf && pacman -Sy
   fi
